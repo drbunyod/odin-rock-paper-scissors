97b782d6c513b623a1dad2bf48553a9c2d068912
Bunyod <89516132+drbunyod@users.noreply.github.com>
2023-10-23 03:30:35 +0500

    Improve UI

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ function playRound(playerSelection, computerSelection) {
     if ((playerSelection === "Rock" && computerSelection === "Paper") ||
                 (playerSelection === "Paper" && computerSelection === "Scissors") ||
                 (playerSelection === "Scissors" && computerSelection === "Rock")) {
+        winnerAnimation(false, true);
         computerScore++;
     } else if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
                 (playerSelection === "Paper" && computerSelection === "Rock") ||
                 (playerSelection === "Scissors" && computerSelection === "Paper")) {
+        winnerAnimation(true, false);
         playerScore++;
+    } else {
+        winnerAnimation(false, false);
     }
 
     playerScoreBoard.textContent = playerScore;
@@ -18,24 +22,59 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function endGame() {
-    // disable buttons
     const allBtns = document.querySelectorAll("button");
     allBtns.forEach(btn => {
         btn.disabled = true;
     });
 
-    // display winner
-    const winnerText = document.querySelector("#winner-text");
+    const winnerResult = document.querySelector("#winner-result");
+    const winnerScore = document.querySelector("#winner-score");
+
     if (playerScore > computerScore) {
-        winnerText.textContent = `YOU WIN! ${playerScore}-${computerScore}`;
+        winnerResult.textContent = "YOU WIN!";
     } else if (playerScore < computerScore) {
-        winnerText.textContent = `YOU LOSE! ${playerScore}-${computerScore}`;
+        winnerResult.textContent = "YOU LOSE!";
     } else {
-        winnerText.textContent = `DRAW! ${playerScore}-${computerScore}`;
+        winnerResult.textContent = "DRAW!";
     }
 
-    const playAgainBtn = document.querySelector("#play-again-button");
-    playAgainBtn.classList.remove("hidden");
+    winnerScore.textContent = `${playerScore}-${computerScore}`;
+
+    document.querySelector(".winner").style.display = "flex";
+}
+
+function winnerAnimation(player, computer) {
+    const playerBoard = document.querySelector(".player");
+    const computerBoard = document.querySelector(".computer");
+
+    if (player) {
+        playerBoard.classList.add("winner-animation");
+        computerBoard.classList.add("loser-animation");
+    } else if (computer) {
+        playerBoard.classList.add("loser-animation");
+        computerBoard.classList.add("winner-animation");
+    } else {
+        playerBoard.classList.add("draw-animation");
+        computerBoard.classList.add("draw-animation");
+    }
+
+    setTimeout(() => {
+        playerBoard.classList.remove("winner-animation", "loser-animation", "draw-animation");
+        computerBoard.classList.remove("winner-animation", "loser-animation", "draw-animation");
+    }, 1000);
+}
+
+function displayChoice(playerChoice, computerChoice) {
+    const choices = {
+        rock: '🪨',
+        paper: '📄',
+        scissors: '✂️'
+    }
+
+    document.querySelector("#player-choice").textContent = choices[playerChoice.toLowerCase()];
+    document.querySelector("#computer-choice").textContent = choices[computerChoice.toLowerCase()];
+    document.querySelector("#player-choice-text").textContent = playerChoice;
+    document.querySelector("#computer-choice-text").textContent = computerChoice;
 }
 
 function getComputerChoice() {
@@ -52,15 +91,27 @@ const computerScoreBoard = document.querySelector("#computer-score");
 
 const rockBtn = document.querySelector("#rock-button");
 rockBtn.addEventListener("click", () => {
-    playRound("Rock", getComputerChoice());
+    let playerChoice = "Rock";
+    let computerChoice = getComputerChoice();
+
+    displayChoice(playerChoice, computerChoice);
+    playRound(playerChoice, computerChoice);
 });
 
 const paperBtn = document.querySelector("#paper-button");
 paperBtn.addEventListener("click", () => {
-    playRound("Rock", getComputerChoice());
+    let playerChoice = "Paper";
+    let computerChoice = getComputerChoice();
+
+    displayChoice(playerChoice, computerChoice);
+    playRound(playerChoice, computerChoice);
 });
 
 const scissorsBtn = document.querySelector("#scissors-button");
 scissorsBtn.addEventListener("click", () => {
-    playRound("Scissors", getComputerChoice());
+    let playerChoice = "Scissors";
+    let computerChoice = getComputerChoice();
+
+    displayChoice(playerChoice, computerChoice);
+    playRound(playerChoice, computerChoice);
 });
